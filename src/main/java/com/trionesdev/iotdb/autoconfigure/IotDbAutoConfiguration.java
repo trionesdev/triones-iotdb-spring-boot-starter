dbacdcf8a31832d68323f1a00552b6e3602d2052
落雁沙 <fengxiaotx@163.com>
2025-03-08 15:12:08 +0800

    feat: iotdb 2.x

--- src/main/java/com/trionesdev/iotdb/autoconfigure/IotDbAutoConfiguration.java
+++ src/main/java/com/trionesdev/iotdb/autoconfigure/IotDbAutoConfiguration.java
@@ -2,11 +2,22 @@ package com.trionesdev.iotdb.autoconfigure;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.iotdb.isession.pool.ITableSessionPool;
 import org.apache.iotdb.session.pool.SessionPool;
+import org.apache.iotdb.session.pool.TableSessionPoolBuilder;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -15,15 +26,38 @@ import org.springframework.context.annotation.Configuration;
 public class IotDbAutoConfiguration {
     private final IotDbProperties iotDbProperties;
 
+    private List<String> nodeUrls() {
+        List<String> nodeUrls;
+        if (ArrayUtils.isNotEmpty(iotDbProperties.getNodeUrls())) {
+            nodeUrls = new ArrayList<>(Arrays.asList(iotDbProperties.getNodeUrls()));
+        } else {
+            nodeUrls = Collections.singletonList(iotDbProperties.getHost() + ":" + iotDbProperties.getPort());
+        }
+        return nodeUrls;
+    }
+
     @Bean
+    @ConditionalOnProperty(prefix = "spring.iotdb", name = "dialect", havingValue = "tree")
     @ConditionalOnMissingBean(SessionPool.class)
     public SessionPool sessionPool() {
         return new SessionPool.Builder()
-                .host(iotDbProperties.getHost())
-                .port(iotDbProperties.getPort())
+                .nodeUrls(nodeUrls())
                 .user(iotDbProperties.getUser())
                 .password(iotDbProperties.getPassword())
                 .maxSize(iotDbProperties.getMaxSize())
+                .build();
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "spring.iotdb", name = {"dialect"}, havingValue = "table")
+    @ConditionalOnMissingBean(ITableSessionPool.class)
+    public ITableSessionPool tableSessionPool() {
+        return new TableSessionPoolBuilder()
+                .nodeUrls(nodeUrls())
+                .user(iotDbProperties.getUser())
+                .password(iotDbProperties.getPassword())
+                .maxSize(iotDbProperties.getMaxSize())
+                .database(iotDbProperties.getDatabase())
                 .build();
     }
 
--- src/main/java/com/trionesdev/iotdb/autoconfigure/IotDbProperties.java
+++ src/main/java/com/trionesdev/iotdb/autoconfigure/IotDbProperties.java
@@ -3,13 +3,22 @@ package com.trionesdev.iotdb.autoconfigure;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.List;
+
 @Data
 @ConfigurationProperties(prefix = "spring.iotdb")
 public class IotDbProperties {
+    private Dialect dialect;
     private int maxSize = 10;
     private String host;
     private int port;
+    private String[] nodeUrls;
     private String user;
     private String password;
+    private String database;
 
+    public enum Dialect {
+        TREE,
+        TABLE
+    }
 }
